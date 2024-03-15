b9f3e81713634fb288f24925fb067bf9fc1fb69e
Vakum <vakummail@gmail.com>
2024-03-15 11:58:15 +0200

    hot fix

--- src/modules/SearchEngines/Dork/inputValidations/fileTypeValidation.ts
+++ src/modules/SearchEngines/Dork/inputValidations/fileTypeValidation.ts
@@ -1,6 +1,9 @@
 import { ValidationStatus } from '../stores/Dork.store';
 
 export function fileTypeValidation(value: string): ValidationStatus {
+  if (value === '') {
+    return ValidationStatus.true;
+  }
   const isValidExtension = /^[a-zA-Z0-9_-]{2,10}$/.test(value) && !/^\d+$/.test(value);
   return isValidExtension ? ValidationStatus.true : ValidationStatus.false;
 }
